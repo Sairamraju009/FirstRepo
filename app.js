12a21b04424c67f3d835b6eb9f504c8d01f3f4ac
Sairamraju009 <112251006+Sairamraju009@users.noreply.github.com>
2022-08-27 21:12:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const openFace = document.querySelector('.open');
 const closedFace = document.querySelector('.closed');
+const openFace1 = document.querySelector('.open1');
+const closedFace1 = document.querySelector('.closed1');
 const circle = document.querySelector('#circle');
 
 
 //Add event Listner
+//monkey js
 closedFace.addEventListener('mouseleave', ()=>{
     if(openFace.classList.contains('open')){
         openFace.classList.add('active');
@@ -16,6 +19,24 @@ openFace.addEventListener('mouseenter', ()=>{
         openFace.classList.remove('active');
     }
 });
+
+//test
+closedFace1.addEventListener('mouseleave', ()=>{
+    if(openFace1.classList.contains('open1')){
+        openFace1.classList.add('active1');
+        closedFace1.classList.remove('active1');
+    }
+});
+openFace1.addEventListener('mouseenter', ()=>{
+    if(closedFace1.classList.contains('closed1')){
+        closedFace1.classList.add('active1');
+        openFace1.classList.remove('active1');
+    }
+});
+
+
+//circle js
+
 circle.addEventListener('mouseenter', ()=>{
     if(!circle.classList.contains('hover')){
         circle.classList.add('hover');
