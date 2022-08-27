1cc0137eedbbb76a15867f486131c2aa4a5c66ce
Sairamraju009 <112251006+Sairamraju009@users.noreply.github.com>
2022-08-27 20:41:43 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
+const openFace = document.querySelector('.open');
+const closedFace = document.querySelector('.closed');
 const circle = document.querySelector('#circle');
 
+
+//Add event Listner
+closedFace.addEventListener('mouseleave', ()=>{
+    if(openFace.classList.contains('open')){
+        openFace.classList.add('active');
+        closedFace.classList.remove('active');
+    }
+});
+openFace.addEventListener('mouseenter', ()=>{
+    if(closedFace.classList.contains('closed')){
+        closedFace.classList.add('active');
+        openFace.classList.remove('active');
+    }
+});
 circle.addEventListener('mouseenter', ()=>{
     if(!circle.classList.contains('hover')){
         circle.classList.add('hover');
